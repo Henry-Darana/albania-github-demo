fa3f83fa7d98ded2f4831e505fe5c8f526fc0672
Henry Darana <henry.darana@tray.com>
2021-03-25 12:25:24 +0530

    Added One Class, Modified Home Base.

--- tray-github/src/main/java/com/home/HomeBase.java
+++ tray-github/src/main/java/com/home/HomeBase.java
@@ -1,9 +1,14 @@
 package com.home;
 
+import com.one.OneClass;
+
 public class HomeBase {
 
 	public static void main(String[] args) {
 		System.out.println("Kicking Off Project.");
+
+		OneClass oneClass = new OneClass();
+		oneClass.getOutput();
 	}
 
 }
---
+++ tray-github/src/main/java/com/one/OneClass.java
@@ -0,0 +1,9 @@
+package com.one;
+
+public class OneClass {
+
+	public void getOutput() {
+		System.out.println("One Class");
+	}
+
+}
